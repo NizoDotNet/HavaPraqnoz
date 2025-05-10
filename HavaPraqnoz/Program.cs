749dcd2348041b77c4d4a73f762fa53efcf6d605
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-05-10 12:23:26 +0400

    Add project files.

---
+++ HavaPraqnoz/Program.cs
@@ -0,0 +1,31 @@
+using HavaPraqnoz.Services.Abstractions;
+using Refit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddRefitClient<IWeatherClient>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("http://api.weatherapi.com/v1/")); 
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/search/{query}", async (string query, IWeatherClient weatherClient, IConfiguration configuration) =>
+{
+    return await weatherClient.SearchAsync(configuration["ApiKey"] ?? throw new NullReferenceException("API KEY IS NULL, PASS API KEY!"), query);
+});
+
+app.MapGet("/forcast/{id}", async (int id, IWeatherClient weatherClient, IConfiguration configuration) =>
+{
+    return await weatherClient.GetAsync(configuration["ApiKey"] ?? throw new NullReferenceException("API KEY IS NULL, PASS API KEY!"), id);
+});
+app.Run();
---
+++ HavaPraqnoz/Services/Abstractions/IWeatherClient.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+
+namespace HavaPraqnoz.Services.Abstractions;
+
+public interface IWeatherClient
+{
+    [Get("/search.json?key={key}&q={query}")]
+    Task<dynamic> SearchAsync(string key, string query);
+    [Get("/forecast.json?key={key}&q=id:{id}&days=3&aqi=no&alerts=no")]
+    Task<dynamic> GetAsync(string key, int id);
+}
